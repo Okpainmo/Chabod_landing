e850c58f21acb0797d63a6d5358bcf2d7d920c39
Okpainmo <okpainmoandrew@gmail.com>
2021-12-30 09:02:25 +0100

    more progress

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict';
 
 // variables
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
-const autoprefixer = require("gulp-autoprefixer");
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
 
 // custom-hello task
-gulp.task("hello", function (done) {
-  console.log("hello from gulp");
+gulp.task('hello', function (done) {
+  console.log('hello from gulp');
   done();
 });
 
 // sass task
 function buildStyles() {
   return gulp
-    .src("Sass/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("Dist/Styles"));
+    .src('Sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('Dist/Styles'));
 }
 
 exports.buildStyles = buildStyles;
@@ -24,15 +24,15 @@ exports.buildStyles = buildStyles;
 // autoprefixer task
 exports.default = () =>
   gulp
-    .src("Sass/**/*.scss")
+    .src('Sass/**/*.scss')
     .pipe(
       autoprefixer({
         cascade: false,
       })
     )
-    .pipe(gulp.dest("Dist/Styles"));
+    .pipe(gulp.dest('Dist/Styles'));
 
 // watch task
-gulp.task("watch", function () {
-  gulp.watch("Sass/**/*.scss", gulp.series("buildStyles"));
+gulp.task('watch', function () {
+  gulp.watch('Sass/**/*.scss', gulp.series('buildStyles'));
 });
